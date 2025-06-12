e7f1d4d4a968a7b3e18ed75d4330a5682d9fd4eb
hadyannabil <hadyannabilsk@gmail.com>
2025-06-13 00:37:03 +0700

    tes

--- models/Buku.java
+++ models/Buku.java
@@ -1,6 +1,69 @@
 package models;
-public class Buku {
-    public Buku() {
 
+import java.util.Set;
+
+public class Buku {
+    private String kodeBuku;
+    private String namaBuku;
+    private Set<String> penulis;
+    private int stok;
+    private int tahunTerbit;
+    private String kategori;
+
+    public Buku(String kodeBuku, String namaBuku, Set<String> penulis, int stok, int tahunTerbit, String kategori) {
+        this.kodeBuku = kodeBuku;
+        this.namaBuku = namaBuku;
+        this.penulis = penulis;
+        this.stok = stok;
+        this.tahunTerbit = tahunTerbit;
+        this.kategori = kategori;
+    }
+
+    public String getKodeBuku() {
+        return kodeBuku;
+    }
+    public String getNamaBuku() {
+        return namaBuku;
+    }
+    public void setNama(String nama) {
+        this.namaBuku = nama;
+    }
+    public Set<String> getPenulis() {
+        return penulis;
+    }
+    public void setPenulis(Set<String> penulis) {
+        this.penulis = penulis;
+    }
+    public int getStok() {
+        return stok;
+    }
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+    public void tambahStok() {
+        this.stok++;
+    }
+    public void kurangiStok() {
+        if (this.stok > 0) {
+            this.stok--;
+        }
+    }
+    public int getTahunTerbit() {
+        return tahunTerbit;
+    }
+    public void setTahun(int tahun) {
+        this.tahunTerbit = tahun;
+    }
+    public String getKategori() {
+        return kategori;
+    }
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+    public void getInfo() {
+        System.out.println(this.toString());
+    }
+    public String toString() {
+        return "";
     }
 }
