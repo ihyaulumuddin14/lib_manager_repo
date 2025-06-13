c4d38934475aa3cd4c6beb725e05c86f9ed56414
gathan16 <muhgatraka@gmail.com>
2025-06-13 13:44:13 +0700

    menyamakan di atribut&method di class diagram

--- ManajemenMahasiswa.java
+++ ManajemenMahasiswa.java
@@ -1,5 +1,27 @@
 import services.MahasiswaService;
+import models.Mahasiswa;
+import java.util.Map;
+import filehandler.FileHandlerMahasiswa;
+
 
 public class ManajemenMahasiswa extends MahasiswaService {
-    
+    private Map<String, Mahasiswa> daftarMahasiswa;
+    FileHandlerMahasiswa fhMahasiswa;
+
+    public ManajemenMahasiswa(Map<String, Mahasiswa> daftarMahasiswa) {
+        this.daftarMahasiswa = daftarMahasiswa;
+        this.fhMahasiswa = new FileHandlerMahasiswa();
+    }
+    public void tambahMhs(Mahasiswa mhs){
+
+    }
+    public Mahasiswa cariMhs(String nim){
+        return null;
+    }
+    public void editMhs(Mahasiswa mhs){
+
+    }
+    public void hapusMhs(String nim){
+
+    }
 }
--- filehandler/FileHandlerMahasiswa.java
+++ filehandler/FileHandlerMahasiswa.java
@@ -1,3 +1,23 @@
 package filehandler;
+import models.Mahasiswa;
+import java.nio.file.attribute.FileAttributeView;
+import java.util.Map;
+
+public class FileHandlerMahasiswa {
+    private String NAMA_FILE = "mahasiswa.txt";
+    private String DELIMITER = ",";
+    private String DAFTAR_BUKU_DELIMITER = ";";
+
+    public FileHandlerMahasiswa() {
+
+    }
+    public void simpanData(Map<String, Mahasiswa> daftarMahasiswa) {
+
+    }
+    public Map<String, Mahasiswa> bacaData() {
+        return null;
+    }
+}
+
 
 
